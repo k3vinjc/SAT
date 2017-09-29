9521e67f92abc44082f90392e1cd18d56bccb1e4
kevinj <kevinj@kevinj-HP-14-Notebook-PC>
2017-09-29 11:44:35 -0600

    Clase SAT completada

--- SAT/src/main/java/clases/Declaracion.java
+++ SAT/src/main/java/clases/Declaracion.java
@@ -32,6 +32,7 @@ public class Declaracion {
             this.fecha_declaracion = fecha_declaracion.trim();
             validarMarcaLinea(marca, linea);
             txt_error = "";
+            this.cod_declaracion=crearDeclaracion();
         }
     }
 
@@ -66,13 +67,13 @@ public class Declaracion {
         } else if ((consulta = DB.select("max(cod_manifiesto) as codman", "manifiesto", "")) == null) {
             txt_error = "Error de ejecución de consulta de manifiesto después de crearlo.";
         } else if (consulta.isEmpty()) {
-            txt_error = "No se creó la el manifiesto con los datos:  \nMarca: " + marca.cod_marca + "\nLinea: " + linea.cod_linea + "\nModelo: " + modelo + "\nFecha:'" + fecha_declaracion + "'\nPais Origen:'" + pais_origen + "'";
+            txt_error = "No se creó la el manifiesto con los datos: \nMarca: " + marca.cod_marca + "\nLinea: " + linea.cod_linea + "\nModelo: " + modelo + "\nFecha:'" + fecha_declaracion + "'\nPrecio: " + precio;
         } else {
             try {
                 cod_declaracion = ((java.sql.ResultSet) (consulta.get(0))).getInt("codman");
                 return cod_declaracion;
             } catch (Exception ex) {
-                txt_error = "Campo \"cod_linea\" o \"factor\" incorrecto o no encontrado en consulta posterior a supuesta creación de Linea.";
+                txt_error = "Campo \"codman\" incorrecto en consulta posterior a supuesta creación de Manifiesto.";
                 ex.printStackTrace();
             }
         }
